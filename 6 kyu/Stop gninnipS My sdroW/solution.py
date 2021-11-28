249c7a82015f4ceea4bec65673a510f4e9211e5b
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-28 16:14:03 +0200

    Update solution.py

--- 6 kyu/Stop gninnipS My sdroW/solution.py
+++ 6 kyu/Stop gninnipS My sdroW/solution.py
@@ -75,3 +75,6 @@ def spin_words(sentence):
 def spin_words(sentence):
     words = sentence.split()
     return " ".join([word[::-1] if len(word) >= 5 else word for word in words])
+################
+def spin_words(t):
+    return ' '.join(word if len(word) < 5 else word[::-1] for word in t.split())
