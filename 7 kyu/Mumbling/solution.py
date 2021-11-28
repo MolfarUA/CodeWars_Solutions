b7f7cba1713b60874c59a9fa419b519895d2b83d
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-28 16:01:13 +0200

    Update solution.py

--- 7 kyu/Mumbling/solution.py
+++ 7 kyu/Mumbling/solution.py
@@ -3,3 +3,32 @@ def accum(s):
     for i in range(len(s)):
         result += s[i].upper() + s[i].lower() * i + ('-')
     return result[:-1]
+#########################################
+def accum(s):
+    return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
+#########################################
+def accum(s):
+    output = ""
+    for i in range(len(s)):
+        output+=(s[i]*(i+1))+"-"
+    return output.title()[:-1]
+###########################################
+def accum(s):
+    i = 0
+    result = ''
+    for letter in s:
+        result += letter.upper() + letter.lower() * i + '-'
+        i += 1
+    return result[:len(result)-1]
+##########################################
+def accum(s):
+    return "-".join([((j*(i+1)).capitalize()) for i,j in enumerate(s)])
+######################################
+def accum(s):
+    str = ""
+    for i in range(0, len(s)):
+        str += s[i].upper()
+        str += s[i].lower()*i
+        if i != len(s)-1:
+            str += "-"
+    return str
