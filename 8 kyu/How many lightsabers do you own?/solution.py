752a9bde49dfb0092b148469a2cb451f9a97056c
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-28 18:57:38 +0200

    Update solution.py

--- 8 kyu/How many lightsabers do you own?/solution.py
+++ 8 kyu/How many lightsabers do you own?/solution.py
@@ -4,3 +4,37 @@ def how_many_light_sabers_do_you_own(name = 'other'):
     	return 18
     print(0)
     return 0
+#################
+def how_many_light_sabers_do_you_own(name=""):
+    return (18 if name=="Zach" else 0)
+################
+def howManyLightsabersDoYouOwn(*name):
+    return 18 if name == ('Zach',) else 0
+##############
+howManyLightsabersDoYouOwn = lambda n="": 18 * (n=="Zach")
+###############
+def how_many_light_sabers_do_you_own(name = ""):
+    return(18 if name is "Zach" else 0)
+###############
+def how_many_light_sabers_do_you_own(name=''):
+    switch = {      
+        'Zach':  18,
+        }
+    return switch.get(name, 0)
+###############
+def howManyLightsabersDoYouOwn(*name):
+    if name: name = name[0]
+    return 18 if name == 'Zach' else 0
+###############
+def how_many_light_sabers_do_you_own(name=""):
+    return 18 if "Zach" == name else 0
+#############
+def how_many_light_sabers_do_you_own(name="anyone else"):
+    return 18 if name == "Zach" else 0
+#############
+how_many_light_sabers_do_you_own=lambda n='': 18 if n=='Zach' else 0
+############
+def how_many_light_sabers_do_you_own(name=False):
+    return name == "Zach" and 18
+############
+how_many_light_sabers_do_you_own=lambda n=str():0x12.__mul__(sum(map(ord,n)).__eq__(0x186))
