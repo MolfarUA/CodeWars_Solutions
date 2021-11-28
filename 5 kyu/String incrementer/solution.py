a3ce8d3bc5ae54b0f8770302a54cc03efbcd5555
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-29 00:31:06 +0200

    Update solution.py

--- 5 kyu/String incrementer/solution.py
+++ 5 kyu/String incrementer/solution.py
@@ -1,55 +1,9 @@
+import re
+
 def increment_string(strng):
-    if len(strng) < 1:
-        return '1'
-
-    numbers = ''
-    zeros = ''
-
-    if len(strng) == 1 and strng.isnumeric():
-        return str(int(strng) + 1)
-
-
-    for i in range(len(strng)-1, 0, -1):
-        if strng[i].isnumeric():
-            numbers += strng[i]
-        else:
-            break
-
-    if len(numbers) < 1:
-        return strng + '1'
-
-    numbers = numbers[::-1]
-
-    for z in numbers:
-        if z == '0':
-            zeros += '0'
-        else:
-            break
-
-    if len(zeros):
-        result = numbers.replace('0', '', len(zeros))
+    if re.search(r"(\d+)$", strng) is not None:
+        strng = re.sub(r"(\d+)$", lambda x: str(int(x.group(1)) + 1).zfill(len(x.group(1))), strng)
     else:
-        result = numbers
+        strng += "1"
 
-    if len(result) < 1:
-        return strng[0:i+1] + zeros[0:-1] + '1'
-
-    end = (int(result) + 1)
-
-    if strng.isnumeric():
-        if len(numbers) == len(zeros):
-            return strng[0:i] + zeros[0:-1] + '1'
-        elif result.endswith('9') and result.startswith('9'):
-            return strng[0:i] + zeros[0:-1] + str(end)
-        else:
-            return strng[0:i] + zeros + str(end)
-    else:
-        if len(numbers) == len(zeros):
-            return strng[0:i+1] + zeros[0:-1] + '1'
-        elif result.endswith('9') and result.startswith('9'):
-            return strng[0:i+1] + zeros[0:-1] + str(end)
-        else:
-            if len(strng) - len(numbers) == 1:
-                return strng[0] + zeros + str(end)
-            else:
-                return strng[0:i+1] + zeros + str(end)
+    return strng
