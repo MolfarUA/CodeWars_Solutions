794b006a0eb5c872f99d4f184def20e84449c06f
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-28 19:03:41 +0200

    Update solution.py

--- 8 kyu/Convert boolean values to strings 'Yes' or 'No'/solution.py
+++ 8 kyu/Convert boolean values to strings 'Yes' or 'No'/solution.py
@@ -2,3 +2,51 @@ def bool_to_word(boolean):
     if boolean:
         return 'Yes'
     return 'No'
+####################
+def bool_to_word(bool):
+    return "Yes" if bool else "No"
+##################
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+###################
+#Complete the method that takes a boolean value and return a 
+#"Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    if boolean == True:
+        return 'Yes'
+    elif boolean == False:
+        return 'No'
+################
+def bool_to_word(bool):
+    return ['No', 'Yes'][bool]
+#############
+bool_to_word = {True: 'Yes', False: 'No'}.get
+##############
+bool_to_word = ['No','Yes'].__getitem__
+##############
+bool_to_word= lambda _:{1:'Yes'}.get(_,'No')
+###############
+def bool_to_word(boolean):
+    if boolean == True:
+       return str("Yes")
+    elif boolean == False:
+       return str("No")
+    else:
+       print("failed")
+    # TODO
+if __name__ == "__main__":
+  main()   
+#########################
+bool_to_word=lambda b:"YNeos"[1-b::2]
+##################
+bool_to_word=lambda b:"YNeos"[not b::2]
+################
+bool_to_word = lambda b : ["No", "Yes"][b]
+##############
+def bool_to_word(boolean):
+    if boolean == True:
+        var1 = "Yes"
+    elif boolean == False:
+        var1 = "No"
+    return var1
