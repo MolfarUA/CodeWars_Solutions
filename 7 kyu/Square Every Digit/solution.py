c6cc75670017f8a6a4907a9d5a5cbd47cb0aaa3b
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-11-29 15:10:52 +0200

    Update solution.py

--- 7 kyu/Square Every Digit/solution.py
+++ 7 kyu/Square Every Digit/solution.py
@@ -2,10 +2,7 @@ def square_digits(num):
     temp = ""
     for i in str(num):
         temp += str(int(i) ** 2)
-
     return int(temp)
-
-
 print(square_digits(9119))
 #######################
 def square_digits(num):
