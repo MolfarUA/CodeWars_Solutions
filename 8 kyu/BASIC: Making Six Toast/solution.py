57c157287234ae20dbf9751c912c65be220ae093
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-12-07 19:41:24 +0200

    Create solution.py

---
+++ 8 kyu/BASIC: Making Six Toast/solution.py
@@ -0,0 +1,14 @@
+def six_toast(num):
+    return abs(num - 6)
+##########
+six_toast=lambda n:abs(6-n)
+#########
+def six_toast(num):
+    return num-6 if num-6 >= 0 else 6-num
+##########
+six_toast = lambda num: abs(num-6)
+##########
+def six_toast(num):
+    a = 6 - (num) 
+    b = abs(a)
+    return b
