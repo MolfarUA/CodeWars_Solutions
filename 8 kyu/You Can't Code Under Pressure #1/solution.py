74ef311c97863570d27dca242714747425a1bac7
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-12-07 19:47:56 +0200

    Create solution.py

---
+++ 8 kyu/You Can't Code Under Pressure #1/solution.py
@@ -0,0 +1,19 @@
+def double_integer(i):
+    return i * 2
+##########
+def doubleInteger(i):
+    return i << 1
+#########
+def double_integer(i):
+    result = (i * 2)
+    return result
+  
+double_integer(1)
+###########
+def double_integer(i):
+    val = i * 2
+    return val
+    pass
+############
+def double_integer(i):
+    return i // (1/2)
