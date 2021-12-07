56a62eaca2f4ce9b8ed726c6a325cd250e6601f3
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-12-07 19:50:02 +0200

    Create solution.py

---
+++ 8 kyu/Return to Sanity/solution.py
@@ -0,0 +1,9 @@
+def mystery():
+    results = {
+        'sanity': 'Hello'}
+    return results
+#########3
+def mystery():
+    return {'sanity': 'Hello'}
+###########
+mystery=lambda:{'sanity': 'Hello'}
