09fa95ef15a8b0ef7ee46a4270d7280c0d743cff
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-12-09 18:42:42 +0200

    Update solution.js

--- 8 kyu/Area or Perimeter/solution.js
+++ 8 kyu/Area or Perimeter/solution.js
@@ -1,1 +1,36 @@
 const areaOrPerimeter = (l, w) => (l===w) ? l * w : 2 * (l + w)
+###########
+const areaOrPerimeter = (l , w) => l === w ? l*w : 2*(l+w);
+############
+const areaOrPerimeter = function(l , w) {
+  return l == w ? l*w : 2*(l + w)
+};
+###########
+const areaOrPerimeter = function(l , w) {
+  let area = l * w;
+  let perimeter = (l + w) * 2;
+  
+  return l === w ? area : perimeter;
+};
+#############
+const areaOrPerimeter = (l , w) =>
+  l - w ? (l + w) * 2 : l ** 2;
+###########
+const areaOrPerimeter = function(l , w) {
+    const isSquare = l === w ? true : false;
+    return isSquare ? l * w : l * 2 + w * 2;
+};
+############
+const areaOrPerimeter = function(l , w) {
+  let result = 0;
+  l === w ? result = l**2 : result = 2 * (l + w);
+  return result;
+};
+##########
+const areaOrPerimeter = function(l , w) {
+  if(l === w)
+    return l*w;
+  else
+    return (2*l)+(w*2);
+};
+#########
