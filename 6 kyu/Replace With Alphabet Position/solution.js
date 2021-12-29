8c4de461ea2e0f45e97c9734cfd642c63683a933
Molfar <52976230+MolfarUA@users.noreply.github.com>
2021-12-29 23:32:11 +0200

    Create solution.js

---
+++ 6 kyu/Replace With Alphabet Position/solution.js
@@ -0,0 +1,43 @@
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
+
+_______________________________________________
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++){
+    var code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length-1);
+}
+
+_______________________________________________
+let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split('').map(ch => ch.charCodeAt(0) - 64).join(' ');
+
+_______________________________________________
+function alphabetPosition(text) {
+  return text.match(/[a-zA-Z]/g).map( (el) => el.toLowerCase().charCodeAt() - 96 ).join(' ');
+}
+
+_______________________________________________
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.toLowerCase().split('')
+  .filter( letter => {
+    let index = alphabet.indexOf(letter);
+    return index > -1;
+  }  )
+  .map( letter => alphabet.indexOf(letter) + 1 )
+  .join(' ')
+}
+
+_______________________________________________
+function alphabetPosition(text) {
+  return text.toLowerCase().split('').map(a => a.charCodeAt(0) - 96).filter(a => a > 0 && a < 27).join(' ');
+}
