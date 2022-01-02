8cc12cdfc4edd928519dc0dc8004879787372acf
Molfar <52976230+MolfarUA@users.noreply.github.com>
2022-01-02 17:38:16 +0200

    Create solution.cs

---
+++ 8 kyu/Sentence Smash/solution.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Kata
+{
+  public static string Smash(string[] words) =>
+    String.Join(" ", words);
+}
+
+_____________________________________
+public class Kata
+{
+  public static string Smash(string[] words)
+  {
+    return string.Join(" ", words);
+  }
+}
+
+_____________________________________
+using System;
+using System.Linq;
+  
+public class Kata{
+  public static string Smash(string[] words) => String.Join(" ",words);
+}
