3e07d97e7b9c258c65be9fb72158e1392a569c92
Molfar <52976230+MolfarUA@users.noreply.github.com>
2022-06-13 19:26:41 +0300

    Create s.py

---
+++ 8 kyu/Convert a Boolean to a String/s.py
@@ -0,0 +1,78 @@
+def boolean_to_string(b):
+    return str(b)
+
+_________________________________
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    elif b == False:
+        return "False"
+    else:
+        return b
+
+_________________________________
+def boolean_to_string(b):
+    if b is True:
+        return "True"
+    elif b is False:
+        return "False"
+    else:
+        return "invalid imput"
+      
+_________________________________
+def boolean_to_string(b):  
+        return str(b)
+        return str(not b)
+  
+_________________________________
+def boolean_to_string(b):
+    #your code here
+    if b == True:
+        string = "True"
+    else:
+        string = "False"
+    return string
+  
+_________________________________
+def boolean_to_string(b):
+    if(b == True):
+        return("True")
+    elif(b == False):
+        return("False")
+    else:
+        return(nil)
+      
+_________________________________
+def boolean_to_string(b):
+    if b:
+        return "True"
+    else:
+        return "False"
+pass
+
+_________________________________
+def boolean_to_string(b):
+    if b:
+        return "True" 
+    else:
+        return "False"
+
+boolean_to_string(True)
+
+_________________________________
+def boolean_to_string(b):
+    if b==1:
+        b="True"
+        return b
+    else: 
+        b="False"
+        return b
+      
+_________________________________
+def boolean_to_string(b):
+    if b == True:
+        str = 'True'
+    else:
+        str = 'False'
+        
+    return str
