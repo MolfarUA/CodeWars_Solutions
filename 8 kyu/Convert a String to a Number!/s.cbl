2f8a1230c1a45f7b469967aae37acaa6b0c9d356
Molfar <52976230+MolfarUA@users.noreply.github.com>
2022-06-18 19:38:35 +0300

    Create s.cbl

---
+++ 8 kyu/Convert a String to a Number!/s.cbl
@@ -0,0 +1,54 @@
+544675c6f971f7399a000e79
+      
+      
+123456*
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. string-to-number.
+       AUTHOR. "ejini战神".
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 INPUT-VAR    PIC X(06).
+       01 RESULT       PIC S9(06).
+       PROCEDURE DIVISION USING INPUT-VAR RESULT.
+           MOVE INPUT-VAR TO RESULT.
+       END PROGRAM string-to-number.
+_______________________
+123456*
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. string-to-number.
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 INPUT-VAR    PIC X(06).
+       01 RESULT       PIC S9(06).
+       PROCEDURE DIVISION USING INPUT-VAR RESULT.
+           move input-var to result
+           GOBACK.
+       END PROGRAM string-to-number.
+_______________________
+123456*
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. string-to-number.
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 INPUT-VAR    PIC X(06).
+       01 RESULT       PIC S9(06).
+      
+       PROCEDURE DIVISION USING INPUT-VAR
+                                RESULT.
+      
+      * good luck :)
+       move input-var to result
+           GOBACK.
+       END PROGRAM string-to-number.
+_______________________
+123456*
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. string-to-number.
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 S         PIC X(06).
+       01 RES       PIC S9(06).
+       PROCEDURE DIVISION USING S RES.
+         MOVE S TO RES.
+       END PROGRAM string-to-number.
+      
