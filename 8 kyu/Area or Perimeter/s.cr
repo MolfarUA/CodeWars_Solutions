64617b2df098765e7fcc53da96fb4ef2f5c4d103
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-06-21 01:59:15 +0300

    Create s.cr
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Area or Perimeter/s.cr
@@ -0,0 +1,18 @@
+5ab6538b379d20ad880000ab
+
+
+def area_or_perimeter(l , w)
+   l==w ? l*w : 2*(l+w)
+end
+________________________
+def area_or_perimeter(l , w)
+  if l == w
+    l * w
+  else
+    2 * (l + w)
+  end
+end
+________________________
+def area_or_perimeter(l , w)
+  l == w ? l * w : 2 * l + 2 * w
+end
