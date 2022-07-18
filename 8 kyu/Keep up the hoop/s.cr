457a8116e36d8d1d2a43c07c2aa644cf86d45f33
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-19 01:26:08 +0300

    Create s.cr
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Keep up the hoop/s.cr
@@ -0,0 +1,18 @@
+55cb632c1a5d7b3ad0000145
+
+
+def hoop_count(n : Int32) : String
+  n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+end
+_____________________________
+def hoop_count(n)
+  return n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks"
+end
+_____________________________
+def hoop_count(n : Int) : String
+  n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+end
+_____________________________
+def hoop_count(n) : String
+  n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+end
