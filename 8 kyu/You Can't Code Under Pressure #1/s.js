fa8c762cd16c65d45584f84964c0a02c4a345a52
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:32:22 +0300

    Create s.js
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.js
@@ -0,0 +1,10 @@
+53ee5429ba190077850011d4
+
+
+const doubleInteger = i => 2 * i;
+__________________________
+function doubleInteger(i) {
+  return i << 1;
+}
+__________________________
+const doubleInteger = n => n << 1;
