4128029c7a04a69e889fefe5b12b83f65ae0d7bb
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:24:22 +0300

    Create s.cs
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.cs
@@ -0,0 +1,34 @@
+53ee5429ba190077850011d4
+
+
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n)
+  {
+    return n * 2;
+  }
+}
+__________________________
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n) =>n<<1;
+}
+__________________________
+public static class Kata
+{
+    public static int DoubleInteger(int n) => n * 2;
+}
+__________________________
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n)
+  {
+    return 2*n;
+  }
+}
