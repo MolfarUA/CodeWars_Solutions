b0be1c7f21f9da6eaf85cc4b3115c977b3444240
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:33:01 +0300

    Create s.kt
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.kt
@@ -0,0 +1,21 @@
+53ee5429ba190077850011d4
+
+
+package solution
+
+fun doubleInteger(i:Int) = i*2
+__________________________
+package solution
+
+fun doubleInteger(i: Int) = i shl 1
+__________________________
+package solution
+
+fun doubleInteger(i: Int): Int = i * 2
+__________________________
+package solution
+
+fun doubleInteger(i:Int):Int {
+    val i = i * 2
+    return i
+}
