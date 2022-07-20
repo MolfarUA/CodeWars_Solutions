763d011920085659038e8150e750e6118c710499
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:29:31 +0300

    Create s.go
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.go
@@ -0,0 +1,26 @@
+53ee5429ba190077850011d4
+
+
+package kata
+
+func DoubleInteger(i int) int {
+  return 2 * i
+}
+__________________________
+package kata
+
+func DoubleInteger(i int) int {
+  return i * 2
+}
+__________________________
+package kata
+
+func DoubleInteger(i int) int {
+  return i << 1
+}
+__________________________
+package kata
+
+func DoubleInteger(i int) int {
+  return i+i
+}
