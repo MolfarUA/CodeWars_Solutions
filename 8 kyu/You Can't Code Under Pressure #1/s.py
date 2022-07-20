aa952df6a41ed421a8a7acfd4b9ec6aa9bbcb810
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:23:45 +0300

    Update and rename solution.py to s.py
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.py
@@ -0,0 +1,41 @@
+53ee5429ba190077850011d4
+
+
+def double_integer(i):
+    return i * 2
+##########
+def doubleInteger(i):
+    return i << 1
+#########
+def double_integer(i):
+    result = (i * 2)
+    return result
+  
+double_integer(1)
+###########
+def double_integer(i):
+    val = i * 2
+    return val
+    pass
+############
+def double_integer(i):
+    return i // (1/2)
+############
+def double_integer(i):
+    x2 = i * 2
+    return x2
+
+print(double_integer(2))
+############
+def double_integer(i):
+    double_integrer=i*2
+    return double_integrer
+############
+def double_integer(i):
+    a = int( i * 2)
+    return a
+############
+def double_integer(i):
+    doub_int = i * 2
+    return (doub_int)
+double_integer(4)
