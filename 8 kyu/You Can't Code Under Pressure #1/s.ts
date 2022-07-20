92fb41e25d70d493f2164eee3d7f6d9652049edd
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:34:34 +0300

    Create s.ts
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.ts
@@ -0,0 +1,8 @@
+53ee5429ba190077850011d4
+
+
+export const doubleInteger = (i: number): number => i * 2
+__________________________
+export const doubleInteger = (i: number) => 2 * i;
+__________________________
+export const doubleInteger = (i: number): number => i << 1
