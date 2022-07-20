362bf19e8256e0f04d632ea154fb4109da1a1def
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-20 13:30:00 +0300

    Create s.hs
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/You Can't Code Under Pressure #1/s.hs
@@ -0,0 +1,22 @@
+53ee5429ba190077850011d4
+
+
+module DoubleInteger where
+
+doubleInteger :: Num a => a -> a
+doubleInteger = (*2)
+__________________________
+module DoubleInteger where
+
+doubleInteger :: Integer -> Integer
+doubleInteger = (2*)
+__________________________
+module DoubleInteger where
+
+doubleInteger :: Integer -> Integer
+doubleInteger = (*2)
+__________________________
+module DoubleInteger where
+
+doubleInteger :: (Num a) => a -> a
+doubleInteger = (2*)
