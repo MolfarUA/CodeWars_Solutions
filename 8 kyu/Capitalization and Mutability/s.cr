6e634d0497f0b2b3a764a4b4b05042f363dffa5e
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-07-26 22:54:52 +0300

    Create s.cr
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Capitalization and Mutability/s.cr
@@ -0,0 +1,14 @@
+595970246c9b8fa0a8000086
+
+
+def capitalize_word(word : String) : String
+  word.capitalize()
+end
+______________________
+def capitalize_word(word)
+  word[0].upcase()+word[1..-1]
+end
+______________________
+def capitalize_word(word)
+  word.capitalize
+end
