4ec2f305b692407b8d0ce534a5b362839f22097b
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:28:34 +0300

    Create s.c
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.c
@@ -0,0 +1,14 @@
+58261acb22be6e2ed800003a
+
+
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width *height;
+}
+_____________________________
+double getVolumeOfCubiod(double length, double width, double height) {
+    double v;
+    v=length*width*height;
+    return v;
+}
+_____________________________
+const long double getVolumeOfCuboid = 1.5249269223191329542e+59L;
