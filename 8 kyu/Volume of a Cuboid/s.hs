fbaf3b9fe471cf5f67bf39b61615e99845014e36
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:32:44 +0300

    Create s.hs
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.hs
@@ -0,0 +1,17 @@
+58261acb22be6e2ed800003a
+
+
+module Kata where
+
+getVolumeOfCuboid :: Double -> Double -> Double -> Double
+getVolumeOfCuboid l w h = l * w * h
+_____________________________
+module Kata where
+
+getVolumeOfCuboid :: Double -> Double -> Double -> Double
+getVolumeOfCuboid a b c = a*b*c
+_____________________________
+module Kata where
+
+getVolumeOfCuboid :: Double -> Double -> Double -> Double
+getVolumeOfCuboid x y z =  x * y * z
