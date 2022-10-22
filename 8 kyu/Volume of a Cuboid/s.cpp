36d236192d64acc6056f4e9983b3d28a940cad96
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:30:43 +0300

    Create s.cpp
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.cpp
@@ -0,0 +1,21 @@
+58261acb22be6e2ed800003a
+  
+  
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width * height;
+}
+_____________________________
+constexpr double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width * height;
+}
+_____________________________
+double getVolumeOfCuboid(double length, double width, double height) {
+  double cuboid = length * width * height;
+  return cuboid;
+}
+_____________________________
+double getVolumeOfCuboid(double length, double width, double height) {
+  double x;
+  x = length*width*height;
+  return x;
+}
