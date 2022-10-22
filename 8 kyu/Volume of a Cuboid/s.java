3ad62fab474055b53410ef1bcf70e2147415d0be
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:33:15 +0300

    Create s.java
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.java
@@ -0,0 +1,28 @@
+58261acb22be6e2ed800003a
+
+
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    return length * width * height;
+  }
+  
+}
+_____________________________
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    if (length <= 0 || width <= 0 || height <=0) return 0;
+    return length * width * height;
+  }
+  
+}
+_____________________________
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double s, final double w, final double a) {
+    double g = 1;
+    return s*w*a*g;
+  }
+  
+}
