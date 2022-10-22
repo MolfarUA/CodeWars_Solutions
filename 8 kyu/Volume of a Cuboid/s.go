91d78ca99aa119e610be9d8af1bc9ec4076ed296
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:32:06 +0300

    Create s.go
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.go
@@ -0,0 +1,24 @@
+58261acb22be6e2ed800003a
+
+
+package kata
+
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+  return length * width * height
+}
+_____________________________
+package kata
+
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+  var sum float64 = length * width * height
+  return sum
+}
+_____________________________
+package kata
+
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+  return float64(length*width*height)
+}
