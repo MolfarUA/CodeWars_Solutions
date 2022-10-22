5ca79c72cb78bf606a373aaf37e197107cafa86e
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-10-23 00:31:08 +0300

    Create s.cs
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Volume of a Cuboid/s.cs
@@ -0,0 +1,19 @@
+58261acb22be6e2ed800003a
+
+
+public class Kata {
+  public static double getVolumeOfCubiod(double length, double width, double height) => length * width * height;
+}
+_____________________________
+public class Kata {
+  public static double GetVolumeOfCuboid(double l, double w, double h) {
+    return l*w*h;
+  }
+}
+_____________________________
+public class Kata {
+  public static double getVolumeOfCubiod(double length, double width, double height) {
+    double volume = length * width * height;
+    return volume;
+  }
+}
