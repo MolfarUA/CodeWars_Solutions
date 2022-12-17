5e4cd86cbc0e14484e165594f01e1b90c72f87e9
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-12-17 22:22:27 +0200

    Update solution.py
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

--- 8 kyu/BASIC: Making Six Toast/solution.py
+++ 8 kyu/BASIC: Making Six Toast/solution.py
@@ -1,14 +1,30 @@
 def six_toast(num):
     return abs(num - 6)
-##########
+________________________
 six_toast=lambda n:abs(6-n)
-#########
+________________________
 def six_toast(num):
     return num-6 if num-6 >= 0 else 6-num
-##########
+________________________
 six_toast = lambda num: abs(num-6)
-##########
+________________________
 def six_toast(num):
     a = 6 - (num) 
     b = abs(a)
     return b
+________________________
+def six_toast(num):
+    return abs(num - 6)
+print(six_toast(42))
+________________________
+import math
+class codewars_test:
+    def test():
+        print("Cheese")
+
+def six_toast(num):
+    return abs(num-6)
+________________________
+six_toast=lambda t:abs(6-t)
+________________________
+six_toast = lambda x : 6-x if x<=6 else x-6
