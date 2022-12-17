720c9eaa2037267ed69f5da3a76f7af0418d0696
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-12-17 21:40:38 +0200

    Create s.cbl
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/Subtract the Sum/s.cbl
@@ -0,0 +1,24 @@
+56c5847f27be2c3db20009c3
+      
+      
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SUBTRACT-SUM.
+       AUTHOR. "ejini战神".
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 N           PIC 9(8).
+       01 RESULT      PIC a(9).
+       PROCEDURE DIVISION USING N RESULT.
+           MOVE "apple" TO RESULT.
+       END PROGRAM SUBTRACT-SUM.
+_______________________________________
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SUBTRACT-SUM.
+       DATA DIVISION.
+       LINKAGE SECTION.
+       01 N           PIC 9(8).
+       01 RESULT      PIC a(9).
+       PROCEDURE DIVISION USING N RESULT.
+       MOVE 'apple' TO RESULT.
+       END PROGRAM SUBTRACT-SUM.
+
