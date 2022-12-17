cba69baf0c3cdf5a41d93a18a23462c416aad120
MolfarUA <52976230+MolfarUA@users.noreply.github.com>
2022-12-17 22:22:57 +0200

    Create s.cs
    Signed-off-by: MolfarUA <52976230+MolfarUA@users.noreply.github.com>

---
+++ 8 kyu/BASIC: Making Six Toast/s.cs
@@ -0,0 +1,24 @@
+5834fec22fb0ba7d080000e8
+
+
+using System;
+
+public class Kata
+{
+  public static int SixToast(int num) => Math.Abs(6 - num);
+}
+________________________
+using System;
+
+public class Kata
+{
+  public static int SixToast(int num) =>
+    Math.Abs(num - 6);
+}
+________________________
+using System;
+
+public class Kata
+{
+  public static int SixToast(int n) => Math.Abs(n - 6);
+}
